5eb635d0ad33c9842c56f9ffbf9b5e54cc65c876
pedrovp161 <pedrovp161@gmail.com>
2025-01-05 01:41:58 -0300

    .

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r', encoding='utf-8') as arq:
 
 install_requires = "opencv-python>=4.9.0.80", "scanpy>=1.10.1", "scikit-image>=0.24.0"
 
-VERSION = '0.0.03'
+VERSION = '0.0.04'
 DESCRIPTION = 'This comprehensive toolkit enables the analysis of multiple spatial transcriptomics datasets, offering a wide range of analytical capabilities. It supports various types of analyses, including detailed plotting and advanced image analysis, to help you gain deeper insights into your spatial transcriptomics data.'
 
 # Configuração do setup
